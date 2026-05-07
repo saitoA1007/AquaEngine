868c2d911512e27dfc11f2cae86cb23d385a2de8
saitoA1007 <k024g0004@g.neec.ac.jp>
2026-05-08 00:01:43 +0900

    01_04_Menu

--- Project/Resources/Scripts/level_editor.py
+++ Project/Resources/Scripts/level_editor.py
@@ -14,12 +14,55 @@ bl_info = {
 	"category": "Object"
 }
 
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+	# Blenderがクラスを識別するための固有文字
+	bl_idname = "TOPBAR_MT_my_menu"
+	# メニューのラベルとして表示される文字列
+	bl_label = "MyMenu"
+	# 著作権表示の文字列
+	bl_description = "拡張メニュー by " + bl_info["author"]
+
+	# サブメニューの描画
+	def draw(self, context):
+
+		# トップバーの「エディターメニュー」に項目を追加
+		self.layout.operator("wn.url_open_preset",
+			text="Manual", icon='HELP')
+	
+	# 既存のメニューにサブメニューを追加
+	def submenu(self, context):
+
+		# ID指定でサブメニューを追加
+		self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+# Blenderに登録するクラスリスト
+classes = (
+	TOPBAR_MT_my_menu,
+)
+
+# メニュー項目描画
+def draw_menu_manual(self, context):
+	self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
 #アドオン有効化時コールバック
 def register():
+	# Blenderにクラスを登録
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+	# メニュー項目を追加
+	bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
 	print("レベルエディタが有効化されました。")
 
 #アドオン無効化時コールバック
 def unregister():
+	# メニューから項目を削除
+	bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+	# Blenderからクラスを削除
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
 	print("レベルエディタが無効化されました。")
 
 # テスト実行用コード
